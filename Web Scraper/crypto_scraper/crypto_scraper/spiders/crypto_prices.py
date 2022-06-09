42a4bc76c27e60e596a941a3ec2714c425013408
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2022-06-09 20:54:06 +0100

    First Commit, Finished Project

---
+++ Web Scraper/crypto_scraper/crypto_scraper/spiders/crypto_prices.py
@@ -0,0 +1,29 @@
+import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+
+
+class CryptoPricesSpider(CrawlSpider):
+    name = 'crypto_prices'
+    allowed_domains = ['coinmarketcap.com']
+    start_urls = ['https://coinmarketcap.com/historical']
+
+    rules = (
+        Rule(LinkExtractor(restrict_xpaths='//a[@class="historical-link cmc-link"]'), callback='parse_item', follow=True),
+    )
+        # Set restrict_xpaths to the xpath or the links you want to follow
+
+    def parse_item(self, response):
+        for row in response.xpath("//tr[@class='cmc-table-row']"):
+            # This row, is the xpath to a row of the table that we want to access
+
+            yield {
+                'Date': response.xpath("//h1/text()").get(),
+                'Name': row.xpath("./td/div/a[@class='cmc-table__column-name--name cmc-link']/text()").get(),
+                'Symbol': row.xpath("./td/div/a[@class='cmc-table__column-name--symbol cmc-link']/text()").get(),
+                'Price': row.xpath("./td[@class='cmc-table__cell cmc-table__cell--sortable cmc-table__cell--right cmc-table__cell--sort-by__price']/div//text()").get(),
+                'Market Cap': row.xpath("./td[@class='cmc-table__cell cmc-table__cell--sortable cmc-table__cell--right cmc-table__cell--sort-by__market-cap']//text()").get(),
+                'Circulating Supply': row.xpath("./td[@class='cmc-table__cell cmc-table__cell--sortable cmc-table__cell--right cmc-table__cell--sort-by__circulating-supply']//text()").get(),
+
+
+            }
